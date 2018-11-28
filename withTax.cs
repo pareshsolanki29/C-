78c087c1c74710b067ab326fc7d791227d584e57
pareshsolanki29 <bandralites@gmail.com>
2018-11-28 15:15:34 -0500

    morereplt

---
+++ withTax.cs
@@ -0,0 +1,17 @@
+function conert (a){
+  
+var j, i; i=a>=10?'PA':'';/*j=ad(i);*/a*=1+ad(i);return Math.round(a);
+
+
+
+
+}
+function ad(s){
+  var j; j=fst(s);
+  j+=0.01
+  return j/100
+}
+conert(14)
+
+function fst (){
+  return 9.99;}
